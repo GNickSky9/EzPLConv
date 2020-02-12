84f1f9641d46b3b5cab61d1b9137b5138195ebf4
Nick Sky <>
2020-02-12 03:24:16 -0500

    Renamed TEST1.py to Webrobot.py, Added client.py

---
+++ EzPLConv/client.py
@@ -0,0 +1,37 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import sys
+
+def showTracks(playlist,file):
+    for i, item in enumerate(playlist['items']):
+        track = item['track']
+        file.write("%s %s" % (track['artists'][0]['name'] + ' -', track['name']) + '\n')
+
+def getTracks(playlistID,file):
+    clientCredentialsManager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(client_credentials_manager=clientCredentialsManager)
+
+    playlist = sp.playlist(playlistID, fields="tracks,next")
+    tracks = playlist['tracks']
+    showTracks(tracks, file)
+    while tracks['next']:
+        tracks = sp.next(tracks)
+        showTracks(tracks, file)
+
+def main():
+    if len(sys.argv) > 1:
+        playlistID = sys.argv[1]
+    else:
+        print("Need Playlist ID!")
+        print("usage: python " + sys.argv[0] + " [playlist id]")
+        sys.exit()
+
+    output = open("tracks.txt", "w")
+
+    getTracks(playlistID,output)
+
+    output.close()
+
+
+if __name__ == "__main__":
+    main()
